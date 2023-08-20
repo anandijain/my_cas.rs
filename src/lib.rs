c16442b4925f42ce0329c4bcb57ce11e871f078d
anand jain <anandj@uchicago.edu>
2023-08-19 20:41:09 -0700

    calculate differential index of expression.

--- src/lib.rs
+++ src/lib.rs
@@ -1,435 +1,173 @@
-extern crate petgraph;
+use std::collections::HashSet;
 
-use cvode_wrap::{AbsTolerance, LinearMultistepMethod, RhsResult, SolverNoSensi, StepKind};
-use libloading::{Library, Symbol};
-use ndarray;
+use crate::BinOpType::*;
+use crate::Ex::*;
+use fixedbitset::FixedBitSet;
+use ndarray::Array2;
+use petgraph::dot::{Config, Dot};
+use petgraph::visit::GetAdjacencyMatrix;
+use petgraph::{graph::UnGraph, Graph};
 use serde::{Deserialize, Serialize};
-use serde_json;
-use std::collections::HashMap;
-use std::ffi::c_void;
-use std::fs;
-use std::process::Command;
-use std::thread::sleep;
-use std::time::{Duration, Instant};
-use sundials_sys::*;
 
-use petgraph::Graph;
-use petgraph::dot::{Dot, Config};
+type Node = String;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Ex {
     Const(f64),
-    Var(String), // implicitly dependence on time
-    Par(String), // independent of t
-    Mul(Box<Ex>, Box<Ex>),
-    Add(Box<Ex>, Box<Ex>),
-    Pow(Box<Ex>, Box<Ex>),
+    Var(String), // E.g., "x", "y" - usually for variables
+    Par(String), // E.g., "a", "b" - for parameters
+    BinaryOp(BinOpType, Box<Ex>, Box<Ex>),
+    UnaryOp(UnaryOpType, Box<Ex>),
     Der(Box<Ex>), // Represents differentiation wrt time
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub enum EquationType {
-    ODE { order: usize }, // order represents the highest order of differentiation in the equation.
-    AE,                   // Algebraic Equation
+impl Ex {
+    pub fn constant(val: f64) -> Self {
+        Ex::Const(val)
+    }
+
+    pub fn var(name: &str) -> Self {
+        Ex::Var(name.to_string())
+    }
+
+    pub fn par(name: &str) -> Self {
+        Ex::Par(name.to_string())
+    }
+
+    pub fn binop(op: BinOpType, lhs: Ex, rhs: Ex) -> Self {
+        Ex::BinaryOp(op, Box::new(lhs), Box::new(rhs))
+    }
+
+    pub fn unop(op: UnaryOpType, operand: Ex) -> Self {
+        Ex::UnaryOp(op, Box::new(operand))
+    }
+
+    pub fn der(expr: Ex) -> Self {
+        Ex::Der(Box::new(expr))
+    }
+
+    pub fn differential_index(&self) -> usize {
+        match self {
+            Ex::Const(_) | Ex::Var(_) | Ex::Par(_) => 0,
+            Ex::BinaryOp(_, left, right) => {
+                std::cmp::max(left.differential_index(), right.differential_index())
+            }
+            Ex::UnaryOp(_, operand) => operand.differential_index(),
+            Ex::Der(operand) => 1 + operand.differential_index(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Equation {
-    pub variable: String, // the primary variable of the equation (e.g., x, y, etc.)
-    pub expression: Ex,
-    pub eq_type: EquationType,
+pub enum BinOpType {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    Mod, // Modulus
+    Log, // Logarithm base left of right: e.g., log_2(8)
+    Max, // Maximum of two numbers
+    Min, // Minimum of two numbers
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum UnaryOpType {
+    Sin,
+    Cos,
+    Tan,
+    ASin,  // Arcsine
+    ACos,  // Arccosine
+    ATan,  // Arctangent
+    Sinh,  // Hyperbolic Sine
+    Cosh,  // Hyperbolic Cosine
+    Tanh,  // Hyperbolic Tangent
+    Exp,   // Exponential function
+    Ln,    // Natural Logarithm
+    Abs,   // Absolute Value
+    Sqrt,  // Square Root
+    Ceil,  // Ceiling function
+    Floor, // Floor function
+    Neg,   // Negation
+    Fact,  // Factorial
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct System {
-    pub equations: Vec<Equation>,
+    pub equations: Vec<Ex>, // rhs - lhs
     pub defaults: Vec<(String, f64)>,
     pub tspan: (f64, f64),
 }
 
-/// ODE and ODEsystem are old
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct ODE {
-    pub variable: String,
-    pub expression: Ex,
+impl System {
+    /// Computes the differential index for each equation in the system
+    pub fn differential_indices(&self) -> Vec<usize> {
+        self.equations
+            .iter()
+            .map(|eq| eq.differential_index())
+            .collect()
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct ODESystem {
-    pub odes: Vec<ODE>,
-    pub defaults: Vec<(String, f64)>,
-    pub tspan: (f64, f64),
+pub struct Equation {
+    pub lhs: Ex,
+    pub rhs: Ex,
 }
 
-pub fn translate_expression_to_code(
-    expr: &Ex,
-    var_mapping: &HashMap<String, usize>,
-    par_mapping: &HashMap<String, usize>,
-) -> String {
-    match expr {
-        Ex::Const(c) => {
-            if c.fract() == 0.0 {
-                format!("{:.1}", c)
+pub fn build_bipartite_graph(system: &System) -> UnGraph<Node, ()> {
+    let mut graph = UnGraph::<Node, ()>::new_undirected();
+
+    // Add equations to the graph as nodes.
+    for (index, eq) in system.equations.iter().enumerate() {
+        let eq_node = format!("Eq{}", index + 1);
+        let eq_node_idx = graph.add_node(eq_node);
+
+        // Extract variables from the equation.
+        let mut variables = HashSet::new();
+        extract_variables(&eq, &mut variables);
+
+        for var in &variables {
+            let var_node_idx = if let Some(idx) = graph.node_indices().find(|&n| graph[n] == *var) {
+                idx
             } else {
-                c.to_string()
+                graph.add_node(var.clone())
+            };
+            graph.add_edge(eq_node_idx, var_node_idx, ());
+        }
+    }
+
+    graph
+}
+
+// A recursive function to extract variables from an expression.
+pub fn extract_variables(expr: &Ex, variables: &mut HashSet<String>) {
+    match expr {
+        Var(name) => {
+            variables.insert(name.clone());
+        }
+        BinaryOp(_, left, right) => {
+            extract_variables(left, variables);
+            extract_variables(right, variables);
+        }
+        Der(inner) => {
+            extract_variables(inner, variables);
+        }
+        _ => {}
+    }
+}
+
+pub fn convert_to_ndarray(fbs: &FixedBitSet, n: usize) -> Array2<bool> {
+    let mut array = Array2::from_elem((n, n), false);
+
+    for i in 0..n {
+        for j in 0..n {
+            let index = i * n + j;
+            if fbs[index] {
+                array[[i, j]] = true;
             }
         }
-        Ex::Var(v) => {
-            let idx = var_mapping
-                .get(v)
-                .expect(&format!("Variable {} not found", v));
-            format!("y[{}]", idx)
-        }
-        Ex::Par(p) => {
-            let idx = par_mapping
-                .get(p)
-                .expect(&format!("Parameter {} not found", p));
-            format!("p[{}]", idx)
-        }
-        Ex::Mul(a, b) => format!(
-            "({} * {})",
-            translate_expression_to_code(a, var_mapping, par_mapping),
-            translate_expression_to_code(b, var_mapping, par_mapping)
-        ),
-        Ex::Add(a, b) => format!(
-            "({} + {})",
-            translate_expression_to_code(a, var_mapping, par_mapping),
-            translate_expression_to_code(b, var_mapping, par_mapping)
-        ),
-        Ex::Pow(a, b) => format!(
-            "({}.powf({}))",
-            translate_expression_to_code(a, var_mapping, par_mapping),
-            translate_expression_to_code(b, var_mapping, par_mapping)
-        ),
-        _ => "".to_string(),
-    }
-}
-
-pub fn generate_function_from_system(system: &ODESystem) -> String {
-    let state_count = system.odes.len();
-
-    // Generate mapping for variables and parameters
-    let mut var_mapping = HashMap::new();
-    for (idx, ode) in system.odes.iter().enumerate() {
-        var_mapping.insert(ode.variable.clone(), idx);
-    }
-    let mut par_mapping = HashMap::new();
-    let mut idx = 0;
-    for (k, _) in &system.defaults {
-        if !var_mapping.contains_key(k) {
-            par_mapping.insert(k.clone(), idx);
-            idx += 1;
-        }
     }
 
-    let param_count = par_mapping.len();
-    println!("var_mapping: {:?}", var_mapping);
-    println!("par_mapping: {:?}", par_mapping);
-
-    let mut result = format!(
-        "#[no_mangle]\npub extern \"C\" fn my_ode_function(t: f64, y: &[f64; {}], ydot: &mut [f64; {}], p: &[f64; {}]) -> () {{\n",
-        state_count, state_count, param_count
-    );
-
-    result += "    let d = [";
-    for ode in &system.odes {
-        result += &format!(
-            "{},",
-            translate_expression_to_code(&ode.expression, &var_mapping, &par_mapping)
-        );
-    }
-    result.truncate(result.len() - 1); // Remove the last comma
-    result += "];\n";
-
-    result += "    for x in 0..y.len() {\n        ydot[x] = d[x];\n    }\n";
-    result += "}";
-    result
-}
-
-pub fn extract_initial_conditions_and_parameters(system: &ODESystem) -> (Vec<f64>, Vec<f64>) {
-    // Extract y0 from the system
-    let y0: Vec<f64> = system
-        .odes
-        .iter()
-        .map(|ode| {
-            system
-                .defaults
-                .iter()
-                .find(|&&(ref var, _)| *var == ode.variable)
-                .map(|&(_, value)| value)
-                .unwrap() // Default to 0.0 if not found (or panic if you prefer)
-        })
-        .collect();
-
-    // Extract parameters from the system
-    let p: Vec<f64> = system
-        .defaults
-        .iter()
-        .filter(|&&(ref var, _)| !system.odes.iter().any(|ode| ode.variable == *var))
-        .map(|&(_, value)| value)
-        .collect();
-
-    (y0, p)
-}
-
-pub fn transform_to_first_order(system: &System) -> System {
-    let mut new_equations = Vec::new();
-    let mut new_defaults = system.defaults.clone();
-
-    for equation in &system.equations {
-        match &equation.eq_type {
-            EquationType::ODE { order } if *order > 1 => {
-                let mut current_var = equation.variable.clone();
-                let mut current_expr = equation.expression.clone();
-
-                for _ in 1..=*order {
-                    let new_var = format!("d_{}", current_var); // new variable name like "d_x", "d_dx", etc.
-                    let new_eq = Equation {
-                        variable: new_var.clone(),
-                        expression: current_expr.clone(),
-                        eq_type: EquationType::ODE { order: 1 },
-                    };
-                    new_equations.push(new_eq);
-                    new_defaults.push((new_var.clone(), 0.0)); // setting default initial condition for new variable
-                    current_var = new_var;
-                    current_expr = Ex::Var(current_var.clone());
-                }
-            }
-            _ => new_equations.push(equation.clone()), // Any other type of equation remains unchanged
-        }
-    }
-
-    System {
-        equations: new_equations,
-        defaults: new_defaults,
-        tspan: system.tspan,
-    }
-}
-
-pub fn simple_ode() {
-    unsafe extern "C" fn rhs(
-        _t: realtype,
-        y: N_Vector,
-        dy: N_Vector,
-        _user_data: *mut c_void,
-    ) -> i32 {
-        *N_VGetArrayPointer(dy) = -*N_VGetArrayPointer(y);
-        0
-    }
-
-    unsafe {
-        let y = N_VNew_Serial(1);
-        *N_VGetArrayPointer(y) = 1.0;
-
-        let mut cvode_mem = CVodeCreate(CV_ADAMS);
-
-        CVodeInit(cvode_mem, Some(rhs), 0.0, y);
-        CVodeSStolerances(cvode_mem, 1e-6, 1e-8);
-
-        let matrix = SUNDenseMatrix(1, 1);
-        let solver = SUNDenseLinearSolver(y, matrix);
-
-        CVodeSetLinearSolver(cvode_mem, solver, matrix);
-        CVodeSetInitStep(cvode_mem, 0.1);
-        let mut t = 0f64;
-        CVode(cvode_mem, 1.0, y, &mut t, CV_NORMAL);
-        // y[0] is now exp(-1)
-
-        let result = (*N_VGetArrayPointer(y) * 1e6) as i32;
-        assert_eq!(result, 367879);
-
-        N_VDestroy(y);
-        CVodeFree(&mut cvode_mem);
-        SUNLinSolFree(solver);
-        SUNMatDestroy(matrix);
-    }
-}
-
-pub fn pendulum_sys() -> System {
-    let x = Ex::Var("x".to_string());
-    let y = Ex::Var("y".to_string());
-    let dx = Ex::Var("dx".to_string());
-    let dy = Ex::Var("dy".to_string());
-    let g = Ex::Par("g".to_string());
-    let L = Ex::Par("L".to_string());
-    let T = Ex::Par("T".to_string());
-
-    // Second-order ODE for x (d^2x/dt^2 = T*x)
-    let d2x_dt2 = Equation {
-        variable: "dx".to_string(),
-        expression: Ex::Mul(Box::new(T.clone()), Box::new(x.clone())),
-        eq_type: EquationType::ODE { order: 2 },
-    };
-
-    // Second-order ODE for y (d^2y/dt^2 = T*y - g)
-    let d2y_dt2 = Equation {
-        variable: "dy".to_string(),
-        expression: Ex::Add(
-            Box::new(Ex::Mul(Box::new(T.clone()), Box::new(y.clone()))),
-            Box::new(Ex::Mul(Box::new(Ex::Const(-1.)), Box::new(g))),
-        ),
-        eq_type: EquationType::ODE { order: 2 },
-    };
-
-    // Constraint for the pendulum length
-    let constraint = Equation {
-        variable: "constraint".to_string(),
-        expression: Ex::Add(
-            Box::new(Ex::Pow(Box::new(x), Box::new(Ex::Const(2.0)))),
-            Box::new(Ex::Add(
-                Box::new(Ex::Pow(Box::new(y), Box::new(Ex::Const(2.0)))),
-                Box::new(Ex::Mul(Box::new(Ex::Const(-1.)), Box::new(L))),
-            )),
-        ),
-        eq_type: EquationType::AE,
-    };
-
-    // Construct the system
-    let pendulum_system = System {
-        equations: vec![d2x_dt2, d2y_dt2, constraint],
-        defaults: vec![
-            ("x".to_string(), 0.0),
-            ("y".to_string(), 1.0),
-            ("dx".to_string(), 0.0),
-            ("dy".to_string(), 0.0),
-            ("g".to_string(), 9.81),
-            ("L".to_string(), 1.0),
-            ("T".to_string(), 0.0), // Tension is not defined initially
-        ],
-        tspan: (0.0, 10.0),
-    };
-    pendulum_system
-}
-
-pub fn lorenz_sys() -> (ODESystem, [f64; 3], [f64; 3]) {
-    let sig = Ex::Par("sigma".to_string());
-    let rh = Ex::Par("rho".to_string());
-    let bet = Ex::Par("beta".to_string());
-
-    let x = Ex::Var("x".to_string());
-    let y = Ex::Var("y".to_string());
-    let z = Ex::Var("z".to_string());
-
-    // ODEs for the Lorenz system
-    let dx_dt = ODE {
-        variable: "x".to_string(),
-        expression: Ex::Mul(
-            Box::new(sig.clone()),
-            Box::new(Ex::Add(
-                Box::new(y.clone()),
-                Box::new(Ex::Mul(Box::new(Ex::Const(-1.)), Box::new(x.clone()))),
-            )),
-        ),
-    };
-
-    let dy_dt = ODE {
-        variable: "y".to_string(),
-        expression: Ex::Add(
-            Box::new(Ex::Mul(
-                Box::new(x.clone()),
-                Box::new(Ex::Add(
-                    Box::new(rh.clone()),
-                    Box::new(Ex::Mul(Box::new(Ex::Const(-1.)), Box::new(z.clone()))),
-                )),
-            )),
-            Box::new(Ex::Mul(Box::new(Ex::Const(-1.)), Box::new(y.clone()))),
-        ),
-    };
-
-    let dz_dt = ODE {
-        variable: "z".to_string(),
-        expression: Ex::Add(
-            Box::new(Ex::Mul(Box::new(x.clone()), Box::new(y.clone()))),
-            Box::new(Ex::Mul(
-                Box::new(Ex::Mul(Box::new(bet.clone()), Box::new(z.clone()))),
-                Box::new(Ex::Const(-1.)),
-            )),
-        ),
-    };
-
-    let sigma = 10.0;
-    let rho = 28.0;
-    let beta = 8.0 / 3.0;
-
-    // Default values and parameters
-    let defaults = vec![
-        ("x".to_string(), 1.0),
-        ("y".to_string(), 1.0),
-        ("z".to_string(), 1.0),
-        ("sigma".to_string(), sigma),
-        ("rho".to_string(), rho),
-        ("beta".to_string(), beta),
-    ];
-
-    // Constructing the Lorenz ODESystem
-    let lorenz_sys = ODESystem {
-        odes: vec![dx_dt, dy_dt, dz_dt],
-        defaults: defaults,
-        tspan: (0.0, 100.0), // As an example, can be adjusted
-    };
-
-    let (y0_vec, p_vec) = extract_initial_conditions_and_parameters(&lorenz_sys);
-    let y0: [f64; 3] = [y0_vec[0], y0_vec[1], y0_vec[2]]; // As an example for size 3
-    let p: [f64; 3] = [p_vec[0], p_vec[1], p_vec[2]]; // As an example for size 3
-
-    println!("{:?}", lorenz_sys);
-    (lorenz_sys, y0, p)
-}
-
-pub fn solve_lorenz() {
-    simple_ode();
-    // Define sigma, rho, and beta parameters
-    let (lorenz_sys, y0, p) = lorenz_sys();
-    let code = generate_function_from_system(&lorenz_sys);
-
-    fs::write("generated_code.rs", code).expect("Unable to write file");
-
-    // Compile the generated code into a shared library
-    Command::new("rustc")
-        .arg("--crate-type=cdylib")
-        .arg("generated_code.rs")
-        .status()
-        .expect("Failed to compile");
-
-    unsafe {
-        let lib = Library::new("libgenerated_code.dylib").expect("Failed to load the library");
-        let func: Symbol<extern "C" fn(f64, &[f64; 3], &mut [f64; 3], &[f64; 3]) -> i32> =
-            lib.get(b"my_ode_function").expect("Function not found");
-
-        let mut solver = SolverNoSensi::new(
-            LinearMultistepMethod::Adams,
-            |t, y, ydot, k| {
-                func(t, y.into(), ydot.into(), k);
-                RhsResult::Ok
-            },
-            0.0,
-            &y0,
-            1e-8,
-            AbsTolerance::scalar(1e-8),
-            p,
-        )
-        .unwrap();
-
-        let ts = ndarray::Array::linspace(0.1, 10.0, 100);
-        println!("0,{:?}", y0);
-        let mut t = 0.1;
-        let mut last_time = Instant::now();
-        let mut steps = 0;
-        let mut seconds = 0; // Keep track of how many seconds have passed
-
-        const N: usize = 200_000; // Adjust based on expected speed
-
-        let start_time = Instant::now();
-
-        // for _ in 0..N {
-        //     let step = solver.step(t as _, StepKind::Normal).unwrap();
-        //     // println!("{:?}", step);
-        //     t += 0.1;
-        // }
-        // let elapsed = start_time.elapsed().as_secs_f64();
-        // let steps_per_second = N as f64 / elapsed;
-        // println!("Steps per second: {}", steps_per_second);
-        // Steps per second: 93171.21751431652 with --release
-        // Steps per second: 27414.59077180507 without
-    }
+    array
 }
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,77 @@
+extern crate ndarray;
 extern crate petgraph;
+extern crate rand;
 
-use cvode_wrap::{AbsTolerance, LinearMultistepMethod, RhsResult, SolverNoSensi, StepKind};
-use libloading::{Library, Symbol};
-use ndarray;
-use serde::{Deserialize, Serialize};
-use serde_json;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::ffi::c_void;
 use std::fs;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+
+use fixedbitset::FixedBitSet;
+use ndarray::Array2;
+use petgraph::dot::{Config, Dot};
+use petgraph::visit::GetAdjacencyMatrix;
+use petgraph::{Graph, graph::UnGraph};
+use rand::Rng;
+
+use cvode_wrap::{AbsTolerance, LinearMultistepMethod, RhsResult, SolverNoSensi, StepKind};
+use libloading::{Library, Symbol};
+use serde::{Deserialize, Serialize};
+use serde_json;
 use sundials_sys::*;
 
-use petgraph::dot::{Config, Dot};
-use petgraph::Graph;
-
+use crate::BinOpType::*;
+use crate::Ex::*;
 use my_cas::*;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum NodeType {
-    Equation(String),
-    Variable(String),
-}
-
-fn construct_bipartite_graph(system: &System) -> Graph<NodeType, ()> {
-    let mut graph = Graph::<NodeType, ()>::new();
-
-    // Iterate over each equation
-    for eq in &system.equations {
-        let eq_node = graph.add_node(NodeType::Equation(eq.variable.clone()));
-
-        // Get variables from the equation's expression
-        let variables = get_variables_from_expression(&eq.expression);
-
-        for var in variables {
-            let var_node = graph.add_node(NodeType::Variable(var));
-
-            // If this variable node isn't already connected to this equation, create an edge
-            if !graph.contains_edge(eq_node, var_node) {
-                graph.add_edge(eq_node, var_node, ());
-            }
-        }
-    }
-
-    graph
-}
-
-fn get_variables_from_expression(expr: &Ex) -> Vec<String> {
-    match expr {
-        Ex::Const(_) => vec![],
-        Ex::Var(v) => vec![v.clone()],
-        Ex::Par(_) => vec![],
-        Ex::Mul(a, b) | Ex::Add(a, b) | Ex::Pow(a, b) => {
-            let mut vars = get_variables_from_expression(a);
-            vars.extend(get_variables_from_expression(b));
-            vars
-        }
-        Ex::Der(e) => get_variables_from_expression(e),
-    }
-}
 
 fn main() {
-    // Example system (populate this)
-    // let (system, u0, p) = lorenz_sys();
-    let sys = pendulum_sys();
-    let graph = construct_bipartite_graph(&sys);
+    let eq1 = Ex::binop(
+        Sub,
+        Ex::binop(Mul, Ex::var("T"), Ex::var("x")),
+        Ex::der(Ex::der(Ex::var("x"))),
+    );
 
-    // Print out the graph in DOT format
+    let eq2 = Ex::binop(
+        Sub,
+        Ex::binop(
+            Sub,
+            Ex::binop(Mul, Ex::var("T"), Ex::var("y")),
+            Ex::par("g"),
+        ),
+        Ex::der(Ex::der(Ex::var("y"))),
+    );
+
+    let eq3 = Ex::binop(
+        Sub,
+        Ex::binop(
+            Add,
+            Ex::binop(Pow, Ex::var("x"), Ex::constant(2.0)),
+            Ex::binop(Pow, Ex::var("y"), Ex::constant(2.0)),
+        ),
+        Ex::binop(Pow, Ex::par("r"), Ex::constant(2.0)),
+    );
+
+    let sys = System {
+        equations: vec![eq1, eq2, eq3],
+        defaults: vec![],
+        tspan: (0.0, 1.0),
+    };
+
+    println!("{:#?}", sys);
+    let graph = build_bipartite_graph(&sys);
+
     println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
     let isb = petgraph::algo::is_bipartite_undirected(&graph, 0.into());
     println!("Is bipartite: {}", isb);
+    let adj_mat = graph.adjacency_matrix();
+    let array = convert_to_ndarray(&adj_mat, graph.node_count());
+    let int_array: Array2<i32> = array.mapv(|x| if x { 1 } else { 0 });
+
+    println!("{:#?}", int_array);
+
+    let diff_idxs = sys.differential_indices();
+    println!("{:?}", diff_idxs);
 }
